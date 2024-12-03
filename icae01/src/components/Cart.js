25268d4bd3922899c470008d49587c90af3f9dfd
Maleesha Rathnayake <139684247+Maleesha2001-malee@users.noreply.github.com>
2024-12-03 12:18:54 +0000

    create total parice in cart

--- icae01/src/components/Cart.js
+++ icae01/src/components/Cart.js
@@ -1,4 +1,5 @@
-export default function Cart() {
+export default function Cart({cart}) {
+    const grandTotl = cart.reduce((total,item )=> total + item.price * item.quty,0);
     
     return (
         <div className="table-container">
@@ -12,11 +13,18 @@ export default function Cart() {
 
                 </thead>
                 {
+                    cart.map((item)=>(
+                        <tr key={item.id}>
+                            <td>{item.name}</td>
+                            <td>{item.qty}</td>
+                            <td>{item.price * item.qty}</td>
+                        </tr>
+                    ))
                     
                 }
                 <tr>
                     <td >Grand Total : </td>
-                    <td colSpan={2}>Grand Total</td>
+                    <td colSpan={2}>{grandTotl}</td>
                 </tr>
             </table>
            
