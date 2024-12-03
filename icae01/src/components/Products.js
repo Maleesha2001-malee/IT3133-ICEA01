a2be22d095699eb54df5e9b452ac185073efbf8c
Maleesha Rathnayake <139684247+Maleesha2001-malee@users.noreply.github.com>
2024-12-03 11:45:51 +0000

    map to flowers

--- icae01/src/components/Products.js
+++ icae01/src/components/Products.js
@@ -15,7 +15,14 @@ export default function Products(){
                 <h4 className="card-title">Buy flowers</h4>
                 <div className="grid-container">
                     {
-                        //product
+                        flowers.map((flower)=>(
+                            <Product 
+                               key={flower.id}
+                               flower={flower}
+                               />
+
+                        )
+                    )
                     }
                 </div>
 
